4fb434357bd7d4983a1040273fbde15cf0470725
Roberto Ríos <roberto@deveahub.com>
2024-01-03 16:07:03 +0100

    feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
-
 import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon(), react()]
+  integrations: [icon(), react(), sitemap()]
 });
